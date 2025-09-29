3da64e0c95c74eb66462a99f1a0473b0ba0d09df
yckim4042 <yckim4042@gmail.com>
2025-09-29 19:21:02 +0900

    launch file edit

--- launch/zed_stereo_inertial.launch.py
+++ launch/zed_stereo_inertial.launch.py
@@ -41,8 +41,8 @@ def generate_launch_description():
         description="Base camera namespace (e.g., zed, zed2, zedx)",
     )
 
-    # Namespace like "/<camera_name>/zed_node"
-    node_namespace = [camera_name, TextSubstitution(text="/zed_node")]
+    # # Namespace like "/<camera_name>/zed_node"
+    # node_namespace = [camera_name, TextSubstitution(text="/zed_node")]
 
     # Parameter file from YAML
     slam_param_file = PathJoinSubstitution([
@@ -58,17 +58,27 @@ def generate_launch_description():
     orbslam3_node = Node(
         package="orbslam3",
         executable="stereo-inertial",  # <-- adjust if your binary name is different
-        prefix ="xterm -e gdb run --args",
+        # prefix ="xterm -e gdb run --args",
         name="orbslam3",
-        namespace=node_namespace,
+        # namespace=node_namespace,
+        namespace=TextSubstitution(text="/orb_slam3"),
         # Pass the same CLI args order as in YAML: vocab, config, rectify, equalize
         arguments=[vocab, config, rectify, equalize],
         parameters=[slam_param_file],
         # Remappings from YAML
         remappings=[
-            ("camera/left",  "left/image_rect_color"),
-            ("camera/right", "right/image_rect_color"),
-            ("imu",          "imu/data"),
+            # ("camera/left",  "left/image_rect_color"),
+            # ("camera/right", "right/image_rect_color"),
+            # ("imu",          "imu/data"),
+            ("camera/left",
+                [TextSubstitution(text="/"), camera_name,
+                 TextSubstitution(text="/zed_node/left/image_rect_color")]),
+            ("camera/right",
+                [TextSubstitution(text="/"), camera_name,
+                 TextSubstitution(text="/zed_node/right/image_rect_color")]),
+            ("imu",
+                [TextSubstitution(text="/"), camera_name,
+                 TextSubstitution(text="/zed_node/imu/data")]),
         ],
         env=dict(os.environ),
     )
